250beb0836c3815510f2b19d57599d517303d97b
Thomas Epperson <tepperson@briskheat.com>
2018-08-22 14:30:44 -0400

    Extract name of MACHINE by using the bitbake command. Now call copy.sh with the image name and the device to copy it to.

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,26 +3,21 @@
 if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
     echo "Usage - $0 a b"
-    echo " a = deployment directory used by bitbake"
+    echo " a = image name"
     echo " b = device to copy to"
     exit 1
 fi
 
+mach=$(bitbake -e | grep "MACHINE_ARCH=" | cut -d'"' -f2)
+machfolder="./tmp/deploy/images/$mach"
+
+calcname="$machfolder/$1-$mach.wic"
+
 for n in $2* ; do umount $n ; done
 
-echo "Deploy dir = $1"
-files="$1/*.wic"
-for f in $files; do
-  if [ -z "$newest" ]; then
-    newest=$f
-  fi
-  if [[ $f -nt $newest ]]; then
-    newest=$f
-  fi
-done
-echo "Temp name2 = $newest"
-#bmaptool create $newest > ./image.bmap
-echo $(sudo bmaptool copy --bmap $1/image.bmap $newest $2)
-#rm ./image.bmap
+echo "Deploy dir = $machfolder"
+
+echo "Temp name2 = $calcname"
+echo $(sudo bmaptool copy --bmap $machfolder/image.bmap $calcname $2)
 
 sfdisk -c $2 1 a2

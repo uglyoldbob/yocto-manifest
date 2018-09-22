6364416a2104757ad80f00928869a091cfd25ff8
Thomas Epperson <thomas.epperson@gmail.com>
2018-09-22 10:22:00 -0400

    Add verification of file sent by downloading it back and verifying the md5sums.

--- scripts/xbox_copy.sh
+++ scripts/xbox_copy.sh
@@ -13,15 +13,46 @@ if [ "$#" -ne 3 ]; then
 fi
 
 FPATH=${3%/*}
-echo "Path is ${FPATH}"
+echo "Destination Path is ${FPATH}"
 FILE=${3##*/}
-echo "Filename is ${FILE}"
+echo "Destination Filename is ${FILE}"
 
-ftp -n $1 << END_SCRIPT
+FPATHSRC=${2%/*}
+
+if [ -z "${FPATHSRC}" ]; then
+  echo "Blank path"
+  FPATHSRC="."
+  FILESRC="$2"
+else
+  echo "Not blank"
+  FILESRC=${2##*/}  
+fi
+echo "Source path is ${FPATHSRC}"
+echo "Source filename is ${FILESRC}"
+
+rm -f ${FPATHSRC}/testfile
+
+OUT=$(ftp -n $1 << END_SCRIPT
 quote USER xbox
 quote PASS xbox
-cd "$FPATH"
-put "$2" "$FILE"
+lcd $FPATHSRC
+cd $FPATH
+binary
+put "$FILESRC" "$FILE"
+get "$FILE" testfile
 quit
 END_SCRIPT
+)
 
+CHECK1=$(md5sum $2 | awk '{print $1;}')
+CHECK2=$(md5sum ${FPATHSRC}/testfile | awk '{print $1;}')
+echo "$OUT"
+
+echo "Output is $CHECK1 $CHECK2"
+
+if [ "$CHECK1" == "$CHECK2" ]; then
+	echo "File transfer is GOOD!"
+else
+	echo "File transfer failed"
+	exit 1
+fi
